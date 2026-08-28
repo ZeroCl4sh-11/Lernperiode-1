030f6455f67ef3ad44470477bab1cefadf6197f4
ZeroCl4sh-11 <d.vargas.im26@stud.bbbaden.ch>
2026-08-28 16:59:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+
+        string zeichen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvexyz0123456789";
+        string passwort = "";
+        Random zufall = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            int zufallsZahl = zufall.Next(zeichen.Length);
+            char zufallsZeichenn = zeichen[zufallsZahl];
+            passwort = passwort + zufallsZeichenn;
+        }
+        Console.WriteLine("Ihr passwort: " + passwort);
+
+    }
+}   
+
+  
+  
