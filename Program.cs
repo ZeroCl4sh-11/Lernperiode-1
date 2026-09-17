a9044a8dc4383ae2729e59f5edf89f390e585bfa
ZeroCl4sh-11 <d.vargas.im26@stud.bbbaden.ch>
2026-09-17 19:47:24 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,63 @@
-﻿
-using System;
+﻿using System;
+using System.ComponentModel.Design;
 
-
-class Programm
-
-
+class Program
 {
     static void Main()
     {
-        
-        string zeichen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvexyz0123456789";
+        string zeichen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         string passwort = "";
         Random zufall = new Random();
 
-        for (int i = 0; i < 10; i++)
+        Console.WriteLine("Wie viele Zeichen soll dein Passwort haben?");
+        int x = Convert.ToInt32(Console.ReadLine());
 
+        if (x > 12)
         {
-            int zufallsZahl = zufall.Next(zeichen.Length);
-            char zufallsZeichenn = zeichen[zufallsZahl];
-            passwort = passwort + zufallsZeichenn;
+            Console.WriteLine("Dein Passwort ist zu lang!");
+            return;
         }
-        Console.WriteLine("Ihr passwort: " + passwort);
 
+        Console.WriteLine("Welche Option willst du?");
+        Console.WriteLine("1 - Normales Passwort");
+        Console.WriteLine("2 - Nur Kleinbuchstaben");
+        Console.WriteLine("3 - Zahlen und Sonderzeichen");
+        Console.WriteLine("4 - Aussprechbare Wörter");
+
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        if (y == 1)
+        {
+            Console.WriteLine("Grossbuchstaben? ja/nein");
+
+            if (Console.ReadLine() == "nein")
+                zeichen = zeichen.ToLower();
+        }
+
+        if (y == 2)
+            zeichen = "abcdefghijklmnopqrstuvwxyz";
+
+        if (y == 3)
+            zeichen = "0123456789!§$%&/()=?";
+
+        if (y == 4)
+        {
+            string[] woerter = { "Haus", "Auto", "Baum", "Apfel", "Katze" };
+
+            for (int i = 0; i < 3; i++)
+                passwort += woerter[zufall.Next(woerter.Length)];
+        }
+        else
+        {
+            for (int i = 0; i < x; i++)
+                passwort += zeichen[zufall.Next(zeichen.Length)];
+        }
+        
+        if (passwort.Length >= 16)
+            Console.WriteLine("Passwortstärke: Stark");
+        else
+            Console.WriteLine("Passwortstärke: Mittel");
+
+        Console.WriteLine("Das Passwort: " + passwort);
     }
-}   
-
-
-  
+}
